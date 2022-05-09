a9ca3ed28cd4bc4cc2d84122a161ff6e22e01a6c
godoty <yxxsxxny@gmail.com>
2022-05-09 21:26:55 +0900

    feat : solve the Monty Hall Problem

---
+++ monty_hall_problem.py
@@ -0,0 +1,13 @@
+import random
+
+doors = ['goat', 'goat', 'car']
+# random으로 doors 순서 변경
+
+# car 골랐을 때 바꾸는 모드 -> 패배
+
+# random하게 숫자 선택
+
+# 바꾸는 모드 / 끝까지 안바꾸는 모드 -> simulation 2개로 안나눠도 된다.
+print('최종결과: 바꾸는 사람 = 66.6%, 안바꾸는 사람 = 33.3%' )
+#%%
+# %%
